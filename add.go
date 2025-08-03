a2b0bc52c2018794a641124e81c8eb1288e865f0
Mazen Saad <mmes3666@gmail.com>
2025-08-03 15:01:51 +0200

    clean up and basic subcmd setup

---
+++ add.go
@@ -0,0 +1,5 @@
+package main
+
+func (cmd *AddCmd) Run(cli *CLI) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -7,19 +7,22 @@ import (
 )
 
 type CLI struct {
-	AwesomeFile string `kong:"short='f',long='awesome-file',help='path to awesome file',default='awesome.yaml'"`
-	Debug       bool   `kong:"long='debug',help='print raw error messages on error'"`
+	AwesomeFile string `kong:"short='f',long='awesome-file',help='path to awesome file.',default='awesome.yaml'"`
+	Debug       bool   `kong:"long='debug',help='print raw error messages on error.'"`
 
-	Generate GenerateCmd `kong:"cmd,help='generate file from template'"`
-	Enrich   EnrichCmd   `kong:"cmd,help='enrich YAML file. On success, a awesome-lock.json file will be created'"`
+	Generate GenerateCmd `kong:"cmd,help='generate file from template.'"`
+	Enrich   EnrichCmd   `kong:"cmd,help='enrich YAML file. On success, a awesome-lock.json file will be created.'"`
+	Add      AddCmd      `kong:"cmd,help='Add item to list.'"`
 }
 
 type GenerateCmd struct {
-	HTML   bool   `kong:"short='H',help='output HTML'"`
-	TemplateFile string `kong:"arg,required,help='path to template file'"`
+	HTML         bool   `kong:"short='H',help='output HTML'"`
+	TemplateFile string `kong:"arg,required,help='path to template file.'"`
 }
 
 type EnrichCmd struct{}
+
+type AddCmd struct{}
 
 func CliErrorf(err error, format string, a ...any) error {
 	if _debugMode {
@@ -38,8 +41,11 @@ func main() {
 	parser := kong.Parse(
 		&cli,
 		kong.Name("awelist"),
-		kong.Description("A CLI tool for managing awesome lists"),
+		kong.Description("A CLI tool for managing awesome lists."),
 		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+		}),
 	)
 
 	_debugMode = cli.Debug
